ce49776280176df32bffb4a7f2aa317e5c575b46
answr42Qu <raphthearrow@gmail.com>
2021-10-06 13:18:51 -0400

    hurry

---
+++ File/ArrayQueue.hpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+
+template <typename T>
+class ArrayQueue {
+private:
+    T* array;
+    size_t arrayLength;
+    size_t frontIndex;
+    size_t count;
+
+public:
+    ArrayQueue(size_t length) {
+        
+    }
+
+    ~ArrayQueue() {
+
+    }
+
+    void push(T data) {
+
+    }
+
+    void pop() {
+
+    }
+
+    T front() {
+
+    }
+
+    T back() {
+
+    }
+
+    size_t size() {
+        
+    }
+}
