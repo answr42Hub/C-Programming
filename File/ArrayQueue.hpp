61f5e9d7a54d661b37599eb6e6ed20b3c66576a7
answr42Qu <raphthearrow@gmail.com>
2021-10-06 13:54:35 -0400

    voila

--- File/ArrayQueue.hpp
+++ File/ArrayQueue.hpp
@@ -10,30 +10,38 @@ private:
 
 public:
     ArrayQueue(size_t length) {
-        
+        array = new T[length];
+        arrayLength = length;
+        count = frontIndex = 0;
     }
 
     ~ArrayQueue() {
-
+        delete[] array;
     }
 
     void push(T data) {
-
+        if(frontIndex < arrayLength) {
+            array[frontIndex = ++frontIndex % arrayLength] = data;
+            count++;
+        } 
     }
 
     void pop() {
-
+        if(count){
+            frontIndex = ++frontIndex % arrayLength;
+            count--;
+        }
     }
 
     T front() {
-
+        return (count) ? array[frontIndex-1] : 0;
     }
 
     T back() {
-
+        return (count) ? array[(frontIndex + count) % arrayLength];
     }
 
     size_t size() {
-        
+        return count;
     }
 }
