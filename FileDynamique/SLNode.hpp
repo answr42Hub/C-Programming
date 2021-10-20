1c7abe2254620c2ab20933d2780964dbaea302ef
answr42 <raphthearrow@gmail.com>
2021-10-20 15:16:00 -0400

    Voila

--- FileDynamique/SLNode.hpp
+++ FileDynamique/SLNode.hpp
@@ -4,7 +4,7 @@ class SLNode {
     T data;
     SLNode<T>* next;
 
-    SLNode(T data, SLNode<T>* next) {
+    SLNode(T data, SLNode<T>* next = nullptr) {
         this->data = data;
         this->next = next;
     }
---
+++ FileDynamique/main.cpp
@@ -0,0 +1,11 @@
+#include "Queue.hpp"
+
+int main() {
+    Queue<int>* fileDynamique = new Queue<int>();
+
+    fileDynamique->push(42);
+    fileDynamique->push(404);
+    fileDynamique->push(666);
+
+return 0;
+}
