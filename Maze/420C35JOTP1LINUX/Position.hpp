83230fbba95883b0cfdb1e08c54a503d1eafeb1c
answr42Hub <raphthearrow@gmail.com>
2021-10-26 15:41:28 -0400

    keep it up

--- Maze/420C35JOTP1LINUX/Position.hpp
+++ Maze/420C35JOTP1LINUX/Position.hpp
@@ -3,12 +3,16 @@
 class Position {
     private:
         char x, y;
-        bool dir[4];
+        bool* dir;
     
     public:
         Position(char x, char y, bool dir[4]) {
             this->x = x;
             this->y = y;
-            this->dir[4] = dir[4];
+            this->dir = dir;
     }
+
+    char getX() {return x;}
+    char getY() {return y;}
+    bool* getDir() {return dir;}
 };
--- Maze/420C35JOTP1LINUX/main.cpp
+++ Maze/420C35JOTP1LINUX/main.cpp
@@ -10,5 +10,7 @@ int main(int argc, char* argv[]) {
 	Application::getInstance()->addWindow(new MazeSolver(new Maze(3)));
 	Application::getInstance()->start();
 
+	
+
 	return 0;
 }
