915f00679df1b503d3360769d5cffb97a682e616
answr42Hub <raphthearrow@gmail.com>
2021-10-25 21:38:28 -0400

    to be finished !

--- Maze/420C35JOTP1LINUX/main.cpp
+++ Maze/420C35JOTP1LINUX/main.cpp
@@ -2,6 +2,9 @@
 #include "MazeSolver.hpp"
 #include "Stack.hpp"
 #include "Position.hpp"
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
 	Application::getInstance()->addWindow(new MazeSolver(new Maze(3)));
