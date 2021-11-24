38c57e0b70d2fd2c930b5123bd8a19633ab9712a
answr42Hub <raphthearrow@gmail.com>
2021-11-24 13:09:25 -0500

    Nouveau TP 3

---
+++ TP3VCLinux/Window.hpp
@@ -0,0 +1,50 @@
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_ttf.lib")
+#pragma comment(lib, "SDL2_image.lib")
+
+#include <cmath>
+#include <string>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+
+using namespace std;
+
+enum Icon { FOLDER, NOTE, HUFFMAN };
+enum Menu { NEW_FOLDER = 1, NEW_NOTE = 2, RENAME = 4, DELETE = 8, ENCODE = 16, DECODE = 32, SELECT_ALL = 64 };
+
+class Window {
+public:
+	Window();
+	~Window();
+
+	static void drawIcon(const Icon& icon, const int& x, const int& y, const bool& selected = false);
+	static void drawString(const string& text, const int& x, const int& y);
+	static int getHeight();
+	static int getIconHeight();
+	static int getIconWidth();
+	static int getStringWidth(const string& text);
+	static int getWidth();
+	static bool open(void (*OnInit)(), void(*OnMenuClick)(const unsigned int&), void(*OnRefresh)(), void(*OnWindowClick)(const int&, const int&, const bool&, const bool&), void (*OnQuit)());
+	static void setTitle(const string& title);
+	static void showMenu(const int& x, const int& y, const unsigned int& enables = 0);
+	static string showTextBox(const string& text = "");
+	static string showTextField(const string& text = "");
+
+private:
+	static TTF_Font* font, * textFont;
+	static unsigned int enableItems;
+	static Window* instance;
+	static bool isMenu;
+	static SDL_Texture* icons, * menu, * menuItems[7], * textField, * textBox, * textTexture;
+	static SDL_Event sdlEvent;
+	static SDL_Rect dstRect, itemRect, menuRect, srcRect, textRect, textFieldRect, textBoxRect;
+	static void(*OnMenuClick)(const unsigned int&);
+	static void(*OnRefresh)();
+	static void(*OnWindowClick)(const int&, const int&, const bool&);
+
+	SDL_Renderer* sdlRenderer;
+	SDL_Window* sdlWindow;
+};
