44d1b08096ed91bd6436a21b03ee5d3c80fe943d
answr42Hub <raphthearrow@gmail.com>
2021-11-26 15:31:32 -0500

    Folder

--- TP3VCLinux/Folder.hpp
+++ TP3VCLinux/Folder.hpp
@@ -12,5 +12,12 @@ private:
 
 public:
 	// TODO : Impl�mentation des m�thodes n�cessaires
-	
+	Folder(string name) {
+		this->name = name;
+	}
+
+	void creatFolder(string name) {
+		folders.push_back(new Folder(name));
+	}
+
 };
