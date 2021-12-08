96cb727197800272e0c0aedb404d85c6f5745b77
answr42Hub <raphthearrow@gmail.com>
2021-12-08 13:37:47 -0500

    oui...

---
+++ B-Tree/B-Tree.hpp
@@ -0,0 +1,9 @@
+/*
+
+- Nouvelle donnée TOUJOURS dans une feuille.
+- Données toujours triées en ordre croissant.
+- Quand un noeud est plein : 
+    -Créer un nouveau noeud avec les données de droite.
+    -Déplacer la donnée du centre au parent.
+    -Mettre à jour les pointeurs.
+*/
